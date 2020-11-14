1cec7c12b11897e3b3a06490d55f27561e139d07
liehuozhizun <hyuan3@ucsc.edu>
2020-11-14 13:14:04 -0800

    rename repository

--- src/main/java/org/ucsccaa/mms/Repositories/OPTRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ucsccaa.mms.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.ucsccaa.mms.domains.OPT;
-
-@Repository
-public interface OPTRepository extends JpaRepository<OPT,Long> {
-}
