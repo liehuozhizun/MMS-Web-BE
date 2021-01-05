dfe9607deedb5429ac6959904e477d5603476ecd
Eirc <dongzihao123@126.com>
2021-01-04 20:02:29 -0800

    CAA-96, fix, allow fix token to pass

--- src/main/java/org/ucsccaa/mms/filters/AuthenticationFilter.java
+++ src/main/java/org/ucsccaa/mms/filters/AuthenticationFilter.java
@@ -2,12 +2,9 @@ package org.ucsccaa.mms.filters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.ucsccaa.mms.models.ServiceResponse;
-import org.ucsccaa.mms.models.Status;
-import org.ucsccaa.mms.services.AuthenticationService;
-import org.ucsccaa.mms.services.MemberService;
 import org.ucsccaa.mms.services.impl.AuthenticationServiceImpl;
 
 import javax.servlet.*;
@@ -23,14 +20,20 @@ public class AuthenticationFilter implements Filter {
     @Autowired
     private AuthenticationServiceImpl authenticationService;
 
+    @Value("${fixtoken}")
+    private String fixtoken;
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
         String authHead = httpServletRequest.getHeader("authorization");
-        System.out.println(authHead.substring(0,6));
+        System.out.println(fixtoken);
         System.out.println(authHead);
+        if (fixtoken.equals(authHead)){
+            chain.doFilter(httpServletRequest,httpServletResponse);
+            return;
+        }
         try {
             if (authHead == null || !"Bearer".equals(authHead.substring(0, 6)) || authenticationService.validateJwtToken(authHead.substring(6))) {
                 httpServletResponse.sendError(500, "INVALID TOKEN");
--- src/main/java/org/ucsccaa/mms/filters/AuthorizationFilter.java
+++ src/main/java/org/ucsccaa/mms/filters/AuthorizationFilter.java
@@ -1,6 +1,7 @@
 package org.ucsccaa.mms.filters;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.ucsccaa.mms.services.AuthorService;
@@ -19,9 +20,15 @@ public class AuthorizationFilter implements Filter {
     private AuthorService authorService;
     @Autowired
     private AuthenticationServiceImpl authenticationService;
+    @Value("${fixtoken}")
+    private String fixtoken;
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final String token = ((HttpServletRequest)request).getHeader("authorization");
+        if (fixtoken.equals(token)){
+            chain.doFilter(request,response);
+            return;
+        }
         String level = authenticationService.loadUserByUsername(authenticationService.getUserNameFromToken(token)).getStaff().getAuthorization().getLevel().toString();
         String method = ((HttpServletRequest) request).getMethod().toUpperCase();
         String uri = ((HttpServletRequest) request).getRequestURI().substring(1).toUpperCase();
